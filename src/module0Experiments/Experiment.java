1afe647cc4458691f55d7a625b10e34e2fac8627
Anand Mangal <anand.mangal94@gmail.com>
2016-06-15 15:19:15 -0700

    Add in lib folder with jMusic JAR and Joplin midi files. Also added experiments module and pseudocode for module 1.

---
+++ src/module0Experiments/Experiment.java
@@ -0,0 +1,50 @@
+package module0Experiments;
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+
+import jm.JMC;
+import jm.music.data.CPhrase;
+import jm.music.data.Part;
+import jm.music.data.Phrase;
+import jm.music.data.Score;
+import jm.util.Read;
+import jm.util.Write;
+
+public class Experiment extends Frame implements JMC {
+
+	public static void main(String[] args) {
+		FileDialog fd;
+		Frame f = new Frame();
+
+		fd = new FileDialog(f, "Choose a MIDI file to analyze or cancel.",
+				FileDialog.LOAD);
+		fd.setVisible(true);
+
+		if (fd.getFile() == null)
+			System.exit(0);
+		
+		Score s = new Score();
+		Read.midi(s, fd.getDirectory() + fd.getFile());
+		s.setTitle(fd.getFile());
+		
+		Part[] arr = s.getPartArray();
+		for(int i = 0; i < arr.length; i++ ){
+			System.out.println("Part " + i + " has " + arr[i].getPhraseArray().length + " phrases.");
+			Phrase[] pArr = arr[i].getPhraseArray();
+			CPhrase chord = new CPhrase(pArr[0].getStartTime());
+			for(int j = 0; j < pArr.length; j++){
+				System.out.println("Phrase " + j);
+				//Write.midi(pArr[j], "Part" + i + "-Phrase" + j + ".mid");
+				chord.addPhrase(pArr[j]);
+				try {
+				    Thread.sleep(1000);
+				} catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+			}
+			Write.midi(chord, "Part " + i + ".mid");
+		}
+	}
+
+}
